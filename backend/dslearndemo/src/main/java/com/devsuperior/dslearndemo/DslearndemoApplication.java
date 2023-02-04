bcfbeb1e5e119d517c8e3e3bf04e705e978badd5
adamcardoso <adamjuniorcardoso@gmail.com>
2023-02-03 23:18:36 -0300

    Backend project created

---
+++ backend/dslearndemo/src/main/java/com/devsuperior/dslearndemo/DslearndemoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslearndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearndemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DslearndemoApplication.class, args);
+    }
+
+}
---
+++ backend/dslearndemo/src/test/java/com/devsuperior/dslearndemo/DslearndemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslearndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearndemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
